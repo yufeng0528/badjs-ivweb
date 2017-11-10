341d8bff51bca1c82025e879d9fe3c83e689f59d
sampsonwang(王少飞) <sampsonwang@tencent.com>
2017-11-10 14:42:59 +0800

    perf: 日志量统计

--- badjs-web/service/get_pv.sh
+++ badjs-web/service/get_pv.sh
@@ -4,7 +4,12 @@
 # read nginx access log file
 
 logs_path="/data/data2/nginx_log/access/"
+logs_path_log="/data/data2/nginx_log/access/"
 
 logs_file=${logs_path}access_$(date -d "yesterday" +"%Y%m%d").log
 
+logs_file_log=${logs_path_Log}access_$(date -d "yesterday" +"%Y%m%d").log
+
 /data/home/server/nodejs/bin/node /data/badjs/badjs-installer/badjs-web/service/handle_pv.js ${logs_file} $(date -d "yesterday" +"%Y%m%d")
+
+/data/home/server/nodejs/bin/node /data/badjs/badjs-installer/badjs-web/service/handle_pv_log.js ${logs_file_log} $(date -d "yesterday" +"%Y%m%d")
