029fdea66bf1a669fb76d067208a19603589afaf
tickli <lizhen55521@gmail.com>
2018-11-26 23:27:58 +0800

    feat: add email time

--- badjs-web/service/EmailService.js
+++ badjs-web/service/EmailService.js
@@ -297,7 +297,7 @@ EmailService.prototype = {
             this.from,
             [user.email],
             [loginUser.email],
-            "【BadJS 授权成功 " + dateFormat(this.date, "yyyy-MM-dd") + "】",
+            "【BadJS 授权成功 " + dateFormat(this.date, "yyyy-MM-dd hh:mm:ss") + "】",
             '<html><h3>用户 ' + user.loginName + ' 申请的 BadJS-id：' + items.applyId+ ' 授权成功</h3></html>'
         );
     },
