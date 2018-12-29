e2373a348b88c85496deaab74c3389d7618e7150
tickli <tickli@tencent.com>
2018-12-29 11:37:17 +0800

    feat: format codes

--- badjs-web/utils/ivwebMail_for_single.js
+++ badjs-web/utils/ivwebMail_for_single.js
@@ -1,10 +1,11 @@
 'use strict';
-const path = require('path')
+
+const path = require('path');
 const nodemailer = require('nodemailer');
-const Promise = require('bluebird')
+const Promise = require('bluebird');
 const log4js = require('log4js');
 const logger = log4js.getLogger();
-global.pjconfig = require(path.join(__dirname, '../project.json'))
+global.pjconfig = require(path.join(__dirname, '../project.json'));
 
 const emailConf = global.pjconfig.email;
 
@@ -47,34 +48,32 @@ module.exports = (from, to, cc, title, content, attachments) => {
         // 间隔时间重试
         setTimeout(() => {
             const cp = require('child_process');
-            cp.exec('/data/server/node/node-v4.2.3-linux-x64/bin/node /data/badjs-ivweb/badjs-web/service/ScoreMail.js >> /data/log/scoreMail.log', (err,
-                if (err) {
-                    logger.error(err)
-                }
-                logger.info(out)
-                logger.info(stderr)
-            })
-        }, 10000)
-    })
-}
+            cp.exec('/data/server/node/node-v4.2.3-linux-x64/bin/node /data/badjs-ivweb/badjs-web/service/ScoreMail.js >> /data/log/scoreMail.log',
+                (err, out, stderr) => {
+                    if (err) {
+                        logger.error(err);
+                    }
+                    logger.info(out);
+                    logger.info(stderr);
+                });
+        }, 10000);
+    });
+};
 
 
 function sendMail (maildata) {
-    debugger
-    console.log('send email ....')
+    console.log('send email ....');
     return new Promise((resolve, reject) => {
         // send mail with defined transport object
         transporter.sendMail(maildata, function (error, info) {
             if (error) {
                 console.log(error);
-                reject(error)
+                reject(error);
 
             } else {
-                resolve(info)
+                resolve(info);
                 logger.info('Message sent: ' + info.response);
             }
         });
-
-    })
-
+    });
 }
