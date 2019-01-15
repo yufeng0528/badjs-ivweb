ce1e84a9059b167f1453a2ed9dfd3f803e0a6cb9
tickli <lizhen55521@gmail.com>
2019-01-15 23:28:42 +0800

    feat: version

--- badjs-web/static/module/log/mod.offlinelog.js
+++ badjs-web/static/module/log/mod.offlinelog.js
@@ -191,7 +191,8 @@ function bindEvent() {
                 uin :$target.siblings('.td-4').text(),
                 ip :$target.siblings('.td-5').text(),
                 agent : $target.siblings('.td-6').attr("title"),
-                source :   $target.siblings('.td-7').html() ,
+                source :   $target.siblings('.td-7').html(),
+                version: $target.siblings('.td-8').text()
             })
         }).on('change', 'selectBusiness', function() {
             var val = $(this).val() - 0;
