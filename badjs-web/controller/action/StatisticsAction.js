49e134316b79143922252874fe625f3c0105b607
eczn <1975845422@qq.com>
2019-01-23 17:28:40 +0800

    feat: test

--- badjs-web/controller/action/StatisticsAction.js
+++ badjs-web/controller/action/StatisticsAction.js
@@ -28,24 +28,24 @@ var StatisticsAction = {
         });
     },
 
-    // findBusinessByUser: function(param, req, res) {
-    //     var user = req.session.user;
-    //     var businessService = new BusinessService();
+    findBusinessByUser: function(param, req, res) {
+        var user = req.session.user;
+        var businessService = new BusinessService();
 
-    //     businessService.findBusinessByUser(user.loginName, function(err, item) {
-    //         if (err) {
-    //             res.json({
-    //                 code: 500,
-    //                 data: err
-    //             });
-    //         } else {
-    //             res.json({
-    //                 code: 200, 
-    //                 data: item
-    //             });
-    //         }
-    //     });
-    // },
+        // businessService.findBusinessByUser(user.loginName, function(err, item) {
+        //     if (err) {
+        //         res.json({
+        //             code: 500,
+        //             data: err
+        //         });
+        //     } else {
+        //         res.json({
+        //             code: 200, 
+        //             data: item
+        //         });
+        //     }
+        // });
+    },
 
     getRate: function(param, req, res) {
           var db = global.models.db,
