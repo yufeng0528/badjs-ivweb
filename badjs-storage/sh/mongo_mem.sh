ed5fcbf02220f1cfcfe23f169ce7426d59922da3
tickli <tickli@tencent.com>
2019-02-18 11:30:33 +0800

    feat: mongo_mem show use GB

--- badjs-storage/sh/mongo_mem.sh
+++ badjs-storage/sh/mongo_mem.sh
@@ -1,1 +1,1 @@
-/data/server/mongodb/mongodb-linux-x86_64-3.0.7/bin/mongo 127.0.0.1:27018/badjs  --eval "printjson(db.stats())"
+/data/server/mongodb/mongodb-linux-x86_64-3.0.7/bin/mongo 127.0.0.1:27018/badjs  --eval "printjson(db.stats(1073741824))"
