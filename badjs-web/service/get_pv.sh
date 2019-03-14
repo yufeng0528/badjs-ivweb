eb87022b8d84c72c2de398aca76c1fbbcffadafd
tickli <tickli@tencent.com>
2019-03-14 22:24:45 +0800

    feat: 升级 node 版本

--- badjs-web/service/get_pv.sh
+++ badjs-web/service/get_pv.sh
@@ -10,6 +10,6 @@ logs_file=${logs_path}access_$(date -d "yesterday" +"%Y%m%d").log
 
 logs_file_log=${logs_path_Log}access_$(date -d "yesterday" +"%Y%m%d").log
 
-/data/server/node/node-v4.2.3-linux-x64/bin/node /data/badjs-ivweb/badjs-web/service/handle_pv.js ${logs_file} $(date -d "yesterday" +"%Y%m%d")
-/data/server/node/node-v4.2.3-linux-x64/bin/node /data/badjs-ivweb/badjs-web/service/handle_pv_log.js ${logs_file_log} $(date -d "yesterday" +"%Y%m%d")
+/data/server/node/node10/bin/node /data/badjs-ivweb/badjs-web/service/handle_pv.js ${logs_file} $(date -d "yesterday" +"%Y%m%d")
+/data/server/node/node10/bin/node /data/badjs-ivweb/badjs-web/service/handle_pv_log.js ${logs_file_log} $(date -d "yesterday" +"%Y%m%d")
 
