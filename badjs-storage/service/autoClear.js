ec41d9c9195de4081bd7c6c69acc7491133bdfea
tickli <lizhen55521@gmail.com>
2019-04-10 00:13:05 +0800

    feat: daily clean mongo

--- badjs-storage/service/autoClear.js
+++ badjs-storage/service/autoClear.js
@@ -8,7 +8,7 @@ var log4js = require('log4js'),
     logger = log4js.getLogger();
 
 
-var url = global.MONGODB.url;
+var url = global.pjconfig.mongodb.url;
 
 var mongoDB;
 // Use connect method to connect to the Server
