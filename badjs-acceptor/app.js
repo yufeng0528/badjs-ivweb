a923ea923600e97da1fd35bc0e47013ca528da28
tickli <lizhen55521@gmail.com>
2019-05-06 17:00:55 +0800

    feat: update uin rules

--- badjs-acceptor/app.js
+++ badjs-acceptor/app.js
@@ -197,7 +197,7 @@ app.use('/badjs/offlineLog', function (req, res) {
         return res.end('invalid id');
     }
 
-    if (!/^[0-9]{5,11}$/.test(offline_log.uin)) {
+    if (!/^\w{4, 40}$/.test(offline_log.uin)) {
         return res.end('invalid uin');
     }
 
