a21dde9eb2161a9795be707c3361902f8276ae1b
tickli <tickli@tencent.com>
2019-05-06 13:46:18 +0800

    feat: add callback

--- badjs-storage/cache/pos.js
+++ badjs-storage/cache/pos.js
@@ -3,7 +3,13 @@ const fs = require('fs');
 
 function mkdir (_path) {
     if (!fs.existsSync(_path)) {
-        fs.mkdir(_path);
+        fs.mkdir(_path, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('mkdir ' + _path + 'success');
+            }
+        });
     }
 }
 
