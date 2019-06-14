58cff78909abcb46172bc9a3b4b8728e5682abcb
jancatpan <jancatpan@tencent.com>
2019-06-14 15:37:51 +0800

    style: format and debug

--- badjs-acceptor/filter/limitTotal.js
+++ badjs-acceptor/filter/limitTotal.js
@@ -10,16 +10,17 @@ const endDate = new Date();
 // 当前下一个整点
 endDate.setHours(endDate.getHours(), 60, 0, 0);
 
-// 每隔1小时清理上报记录
+// 每隔1小时（整点）清理上报记录
 const CLEAR_INTERVAL = 60 * 60 * 1000;
-const runIntervalClear = function () {
-    setInterval(function () {
+const runIntervalClear = function() {
+    setInterval(function() {
+        logger.info(`[${process.pid}] clear report records.`);
         reportRecord = {};
     }, CLEAR_INTERVAL);
 };
 
 logger.info('after ' + (endDate - new Date()) + ' run limit monitor clear');
-setTimeout(function () {
+setTimeout(function() {
     runIntervalClear();
 }, endDate - new Date());
 
@@ -27,9 +28,9 @@ setTimeout(function () {
  * Created by chriscai, 为后面的服务减少压力
  * 限制进程每个小时最大上报 200000
  */
-module.exports = function () {
+module.exports = function() {
     return {
-        process: function (data) {
+        process: function(data) {
             const arr = data.data;
             const id = arr ? arr[0].id : null;
             if (!id) {
@@ -48,7 +49,7 @@ module.exports = function () {
             // 超过阈值后，告警一次，并且丢弃上报
             if (total >= QUANTITY_LIMIT) {
                 if (!reportRecord[id].hasNotify) {
-                    logger.info(`id ${id} total is exceed ${QUANTITY_LIMIT}`);
+                    logger.info(`[${process.pid}] id ${id} total is exceed ${QUANTITY_LIMIT}`);
                     reportRecord[id].hasNotify = true;
                     quantityLimitNotify(id, QUANTITY_LIMIT, false);
                 }
@@ -58,11 +59,11 @@ module.exports = function () {
             // 超过一半阈值后，提示一次异常
             if (total >= QUANTITY_LIMIT / 2) {
                 if (!reportRecord[id].hasHalfNotify) {
-                    logger.info(`id ${id} total is exceed ${QUANTITY_LIMIT / 2}`);
+                    logger.info(`[${process.pid}] id ${id} total is exceed ${QUANTITY_LIMIT / 2}`);
                     reportRecord[id].hasHalfNotify = true;
                     quantityLimitNotify(id, QUANTITY_LIMIT, true);
                 }
             }
-        },
+        }
     };
 };
